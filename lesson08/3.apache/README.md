2adae118143b16607def691a382cbc79fbf8825d
Alex Shangin <alex.shangin.13@gmail.com>
2019-09-10 10:45:53 +0300

    Домашнее задание 8.3

--- lesson08/3.apache/README.md
+++ lesson08/3.apache/README.md
@@ -5,3 +5,12 @@
 3. Дополнить юнит-файл apache httpd возможностьб запустить несколько инстансов сервера с разными конфигами
   Задание необходимо сделать с использованием Vagrantfile и proviosioner shell (или ansible, на Ваше усмотрение) 
 
+#### Решение:
+
+  3. [apache.sh](https://github.com/alexshangin/otus/blob/master/lesson08/3.apache/apache.sh) - скрипт изменения unit-файла apache.
+
+```bash
+[root@otusrepo vagrant]# ss -tnulp | grep httpd
+tcp    LISTEN     0      128      :::8080                 :::*                   users:(("httpd",pid=5762,fd=4),("httpd",pid=5761,fd=4),("httpd",pid=5760,fd=4),("httpd",pid=5759,fd=4),("httpd",pid=5758,fd=4),("httpd",pid=5757,fd=4))
+tcp    LISTEN     0      128      :::80                   :::*                   users:(("httpd",pid=5755,fd=4),("httpd",pid=5754,fd=4),("httpd",pid=5753,fd=4),("httpd",pid=5752,fd=4),("httpd",pid=5751,fd=4),("httpd",pid=5750,fd=4))
+```
--- lesson08/3.apache/apache.sh
+++ lesson08/3.apache/apache.sh
@@ -1,23 +1,45 @@
 #!/usr/bin/env bash
-
+# Apache2 double config start
+install_apache2(){
 yes | yum install -y httpd
+}
 
+create_unit(){
 cp /usr/lib/systemd/system/httpd.service /usr/lib/systemd/system/httpd@.service
 sed -i "s#.*EnvironmentFile=/etc/sysconfig/httpd.*#EnvironmentFile=/etc/sysconfig/httpd-%I#g" /usr/lib/systemd/system/httpd@.service
+}
 
-echo '[UNIT]\nWants=httpd@first.service httpd@second.service' > /etc/sysconfig/httpd.target
-
+create_environment(){
 echo 'OPTIONS=-f conf/first.conf' > /etc/sysconfig/httpd-first
 echo 'OPTIONS=-f conf/second.conf' > /etc/sysconfig/httpd-second
+}
 
+create_apache2_config(){
+#first conf port 80
 cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/first.conf
-cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/second.conf
-
 echo 'PidFile /var/run/httpd-first.pid' >> /etc/httpd/conf/first.conf
+#second conf port 8080
+cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/second.conf
 echo 'PidFile /var/run/httpd-second.pid' >> /etc/httpd/conf/second.conf
 sed -i "s#.*Listen 80.*#Listen 8080#g" /etc/httpd/conf/second.conf
+}
 
+start_apache2(){
 systemctl start httpd@first
 systemctl start httpd@second
+}
 
+listen_port(){
 ss -tnulp | grep httpd
+}
+
+main(){
+install_apache2
+create_unit
+create_environment
+create_apache2_config
+start_apache2
+listen_port
+}
+
+[[ "$0" == "$BASH_SOURCE" ]] && main "$@"
